b855e0579f52b1c9cea4b14fdac66b3532fb1cda
fabianr <fabianr7593@gmail.com>
2024-08-13 13:54:27 -0600

    learning about props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import componentImage from "./assets/components.png";
+
 const arrayElements = ["HTML", "REACT", "JSX", "CSS", "FRONTEND"];
 
 function randomNumber() {
@@ -8,7 +10,7 @@ function Header() {
   const textElement = arrayElements[randomNumber()];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={componentImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {textElement} React concepts you will need for almost any app you are
@@ -18,12 +20,38 @@ function Header() {
   );
 }
 
+function CoreConcept(props){
+  return (
+    <li>
+      <img src={props.imgSrc}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="HTML" 
+              description="HyperText Markup Language" 
+              imgSrc={componentImage}/>
+            <CoreConcept 
+              title="HTML" 
+              description="HyperText Markup Language" 
+              imgSrc={componentImage}/>
+            <CoreConcept 
+              title="HTML" 
+              description="HyperText Markup Language" 
+              imgSrc={componentImage}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
