734884041fe8ca06191e37f62ba47f09450a1748
fabianr <fabianr7593@gmail.com>
2024-08-13 13:33:42 -0600

    dinamic texts

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,17 @@
+const arrayElements = ["HTML", "REACT", "JSX", "CSS", "FRONTEND"];
+
+function randomNumber() {
+  return Math.floor(Math.random() * arrayElements.length);
+}
+
 function Header() {
+  const textElement = arrayElements[randomNumber()];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {textElement} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
